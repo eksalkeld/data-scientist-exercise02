a8401c66aab382adf5e7003e3bf95364a2ee48ef
eksalkeld <eksalkeld@users.noreply.github.com>
2020-09-04 22:30:00 -0400

    constants file
    easily surfaced variables for user to change

---
+++ analytics/constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 19:28:37 2020
+
+@author: eksalkeld
+"""
+
+REPO_DIRECTORY='C://Users//eksalkeld//Documents//GitHub//data-scientist-exercise02'
+REPO_FOLDER='data'
+AVIATION_EXTENSION='AviationData.xml'
+AVIATION_FILE='%s//%s//%s' % (REPO_DIRECTORY,REPO_FOLDER,AVIATION_EXTENSION)
+JSON_FOLDER='%s//%s' % (REPO_DIRECTORY,REPO_FOLDER)
