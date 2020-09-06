9b6953ebc74e9b2e52fbbc5be25ab3fd2e67f504
eksalkeld <eksalkeld@users.noreply.github.com>
2020-09-06 16:13:10 -0400

    fix encoder storage/retrival

--- analytics/model_driver.py
+++ analytics/model_driver.py
@@ -12,15 +12,18 @@ from constants import *
 #Cols for feature selection to consider
 model_cols=[]
 
+#Create the binary target
+df=create_target(df)
+
+#Remove the rows where target is null
+df=df[df['target'].notnull()]
+
 #Convert columns that need to be numeric into floats
 df=convert_numeric(df, cols_to_numeric)
 
 #Turn strings of dates to datetimes and also find year/month/day/day of week
 df=date_processing(df,'EventDate')
 df=date_processing(df,'PublicationDate')
-    
-#Create the binary target
-df=create_target(df)
 
 #Bin the air carriers
 df=encode_carrier(df)
@@ -34,38 +37,52 @@ train=df
 test=df
 val=df
 
+encoders={}
+
 #Find proper encoding for train set
 #Frequency encoding
 for i in cols_to_freq:
+    #Name for storing the encoder
     encoder_name=i+"_encoder"
-    train,encoder_name,model_cols=freq_encode(train,i, model_cols)
+    #Train encoder and transform data
+    train,encoder,model_cols=freq_encode(train,i, model_cols)
+    #Store the encoder
+    encoders[encoder_name]=encoder
     
 #Target encoding
 for i in cols_to_target:
+    #Name for storing the encoder
     encoder_name=i+"_encoder"
-    train,encoder_name,model_cols=target_encode(train,i, model_cols)
+    #Train encoder and transform data
+    df,encoder,model_cols=target_encode(df,i, model_cols)
+    #Store the encoder
+    encoders[encoder_name]=encoder
 
 #WOE encoding
 for i in cols_to_woe:
+    #Name for storing the encoder
     encoder_name=i+"_encoder"
-    train,encoder_name,model_cols=woe_encoder(train,i, model_cols)
+    #Train encoder and transform data
+    train,encoder,model_cols=woe_encoder(train,i, model_cols)
+    #Store the encoder
+    encoders[encoder_name]=encoder
     
 #Process test and validation sets with the encoding
 for j in [test,val]:
     
     #Frequency encoding
     for i in cols_to_freq:
-        encoder_name=i+"_encoder"
+        encoder=encoders[i+"_encoder"]
         j=apply_freq(j,encoder_name)
     
     #Target encoding
     for i in cols_to_target:
-        encoder_name=i+"_encoder"
+        encoder=encoders[i+"_encoder"]
         j=apply_targetenc(j,encoder_name)
 
     #WOE encoding
     for i in cols_to_woe:
-        encoder_name=i+"_encoder"
+        encoder=encoders[i+"_encoder"]
         j=apply_woe(j,encoder_name)
         
 #Feature selection on train set
