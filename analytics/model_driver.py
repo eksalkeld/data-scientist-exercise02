8f27d187cf259d6f6765c18ae3eb7fef2c7c4c2f
eksalkeld <eksalkeld@users.noreply.github.com>
2020-09-07 10:34:42 -0400

    read in data

--- analytics/model_driver.py
+++ analytics/model_driver.py
@@ -8,9 +8,32 @@ from data_clean import *
 from variable_encoding import *
 from constants import *
 
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+#Control console output
+pd.set_option('display.max_columns', None)
+import numpy as np
+np.set_printoptions(threshold=np.nan)
+
+# Load xml file data
+tree = ET.parse(AVIATION_FILE)
+#Initialize list to hold data
+data = []
+#https://stackoverflow.com/questions/41795198/more-efficient-conversion-of-xml-file-into-dataframe
+for el in tree.iterfind('./*'):
+    for i in el.iterfind('*'):
+        data.append(dict(i.items()))
+#Convert list to dataframe
+aviation = pd.DataFrame(data)
+
+df=aviation
 
 #Cols for feature selection to consider
 model_cols=[]
+
+#Convert columns that need to be numeric into floats
+df=convert_numeric(df, cols_to_numeric)
 
 #Create the binary target
 df=create_target(df)
@@ -18,14 +41,18 @@ df=create_target(df)
 #Remove the rows where target is null
 df=df[df['target'].notnull()]
 
-#Convert columns that need to be numeric into floats
-df=convert_numeric(df, cols_to_numeric)
-
-df['NumberOfEngines']=df.NumberOfEngines.fillna(0)
+#Contend with nas in numerical cols
+df=df[df['NumberOfEngines'].notna()]
+#df.loc[:,'NumberOfEngines']=df.NumberOfEngines.fillna(0)
 
 #Turn strings of dates to datetimes and also find year/month/day/day of week
 df=date_processing(df,'EventDate')
-df=date_processing(df,'PublicationDate')
+#df=date_processing(df,'PublicationDate')
+
+#Contend with nas in date representations
+df=df[df['EventDate_month'].notna()]
+df=df[df['EventDate_day'].notna()]
+df=df[df['EventDate_dayofweek'].notna()]
 
 #Bin the air carriers
 df=encode_carrier(df)
@@ -68,26 +95,33 @@ for i in cols_to_woe:
 #Feature selection 
 
 #Categorical features associated with response
-chi_selected,chi_prefixes=chi2select(df,model_cols)
-#PROCESS THE PREFIXES
+chi_selected,chi_with_dummies=chi2select(df,model_cols)
+
+#Include the numeric variables
+model_cols.extend(['NumberOfEngines','EventDate_month','EventDate_day','EventDate_dayofweek'])
 
 #Find predictor columns correlated with each other
-corr_cols=find_corr(df[model_cols])
+corr_cols=find_corr(df[model_cols],cols_to_dummy)
 
 #Vars with lots of missing variables
-miss_cols=missingcount(df)
+miss_cols=missingcount(df[model_cols])
 
 #Important variables from random forest
 rf_vars=rf_imp(df[model_cols],df.target)
 
 #COMBINE: the yeses of chi and rf, the nos of corr and miss
-#model_cols=
+model_cols=[x for x in model_cols if (x in chi_with_dummies or x in rf_vars) and (x not in corr_cols and x not in miss_cols)]
+
+#Scale the features
+dfmodel,scaler=fitscale(df[model_cols],"StandardScaler")
+
+#Add identifier back in
 
 #Train, test, val(?) split
 if no_val_set:
-    X_train, y_train, X_test, y_test=tt_split(df[model_cols],df['target'])
+    X_train, y_train, X_test, y_test=tt_split(dfmodel,df['target'])
 else:
-    X_train, y_train, X_test, y_test, X_val, y_val=ttv_split(df[model_cols],df['target'])
+    X_train, y_train, X_test, y_test, X_val, y_val=ttv_split(dfmodel,df['target'])
 
 
 #Train model
@@ -96,6 +130,8 @@ model, chosenmodel, modelc, modelpenalty, modelweight, modelperf=model_train(X_t
 #Predictions
 train_prob_pred, train_class_pred=model_predict(X_train,model)
 test_prob_pred, test_class_pred=model_predict(X_test,model)
+
+#Coefficients
 
 #Performance
 precision, recall, f1, cm, TN, FP, FN, TP=performance(test_class_pred,y_test)
